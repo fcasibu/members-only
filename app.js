4e7b039f2711295fc250026124a91f9ad34f4d15
nevz <projectnevz@gmail.com>
2022-07-26 00:10:56 +0800

    Move passport strategy to app
    - Added getSignIn middleware for the signIn route
    
    - Added a middleware that assigns req.user to res.locals.currentUser

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const flash = require('connect-flash');
 require('dotenv').config({ path: './config.env' });
 
 mongoose
@@ -21,12 +23,28 @@ db.on('error', () => console.error('failed connecting to mongodb'));
 
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
+const User = require('./models/user');
+const catchAsync = require('./utils/catchAsync');
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: 'email' },
+    catchAsync(async (username, password, done) => {
+      const user = await User.findOne({ email: username });
+      if (!user) return done(null, false, { message: 'User does not exist' });
+      if (!(await user.comparePassword(password)))
+        return done(null, false, { message: 'Invalid Password' });
+
+      return done(null, user);
+    })
+  )
+);
 
 app.use(
   session({
@@ -35,13 +53,28 @@ app.use(
     saveUninitialized: true
   })
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+passport.serializeUser((user, done) => done(null, user.id));
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,29 +1,8 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 
-passport.use(
-  new LocalStrategy(async (email, password, done) => {
-    const user = await User.findOne({ email }).catch(done);
-    if (!user) return done(null, false, { message: 'User does not exist' });
-
-    if (!(await user.comparePassword(password)))
-      return done(null, false, { message: 'Incorrect password' });
-
-    return done(null, user);
-  })
-);
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
-    done(err, user);
-  });
-});
+exports.getSignIn = (req, res) =>
+  res.render('sign-in', { authError: req.flash('error')[0] });
 
 exports.getSignUp = (req, res) => res.render('sign-up');
 
