b46367a1d3b258ee9406798dc22a327feaf481b7
nevz <projectnevz@gmail.com>
2022-07-26 00:12:13 +0800

    Update auth.js
    - Add get route for signIn
    
    - Add post route for signIn and necessary mddlewares

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const passport = require('passport');
 
 const authController = require('../controllers/authController');
 const { isValid } = require('../middlewares/isValid');
-const { validateSignup } = require('../utils/validators');
+const { validateSignup, validateSignin } = require('../utils/validators');
 
 const router = express.Router();
 
-// router.get('/signin');
-// router.post('/signin');
+router.get('/signin', authController.getSignIn);
+router.post(
+  '/signin',
+  validateSignin(),
+  isValid('sign-in'),
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/auth/signin',
+    failureFlash: true
+  })
+);
 
 router.get('/signup', authController.getSignUp);
 router.post(
