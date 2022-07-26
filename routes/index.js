2ef0147d2210056128be1370df774044d0f064e5
nevz <projectnevz@gmail.com>
2022-07-26 16:45:59 +0800

    Update index.js
    - Add get and post request for the join-club route
    
    - Add get and post request for the join-club route
    
    - Add condition for updating a user's role to member or admin

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,41 @@
 const express = require('express');
+const User = require('../models/user');
+const { catchAsync } = require('../utils/catchAsync');
 
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', (req, res, next) => {
+  console.log(req.user);
   res.render('index', { title: 'Express', info: req.flash('info')[0] });
 });
+
+/* Get and post request for the join club route */
+router.get('/join-club', (req, res) => res.render('member'));
+router.post(
+  '/join-club',
+  catchAsync(async (req, res, next) => {
+    if (req.body.answer === process.env.ANSWER_FOR_MEMBER) {
+      await User.findByIdAndUpdate(req.user._id, { role: 'member' });
+      req.flash('info', 'You are now a member!');
+      return res.redirect('/');
+    }
+
+    return res.render('member', { error: { message: 'Incorrect Answer' } });
+  })
+);
+
+/* Get and post request for the admin route */
+router.get('/admin', (req, res) => res.render('admin'));
+router.post(
+  '/admin',
+  catchAsync(async (req, res, next) => {
+    if (req.body.answer === process.env.ANSWER_FOR_ADMIN) {
+      await User.findByIdAndUpdate(req.user._id, { role: 'admin' });
+      req.flash('info', 'You are now an admin!');
+      return res.redirect('/');
+    }
+    return res.render('admin', { error: { message: 'Incorrect Answer' } });
+  })
+);
 
 module.exports = router;
