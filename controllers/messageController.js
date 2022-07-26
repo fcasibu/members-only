115a7878f52bafc23b1cf91c18d1b0146b57f839
nevz <projectnevz@gmail.com>
2022-07-26 21:32:07 +0800

    Update messageController.js
    - Add getAllMessages, this will query for messages sorted by their time of creation and is limited to 10 documents
    
    - Add deleteMessage middleware

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,9 +1,20 @@
 const Message = require('../models/message');
 const { catchAsync } = require('../utils/catchAsync');
+const { paginate } = require('../utils/paginate');
 
-exports.getMessage = (req, res) => {
-  res.render('new-message');
-};
+exports.getAllMessages = catchAsync(async (req, res, next) => {
+  const skip = paginate(req.query.page || 1);
+  const messages = await Message.find()
+    .populate({ path: 'user', select: '-password -email' })
+    .sort({ createdAt: -1 })
+    .skip(skip)
+    .limit(10)
+    .exec();
+
+  res.render('index', { messages, info: req.flash('info')[0] });
+});
+
+exports.getMessage = (req, res) => res.render('new-message');
 
 exports.postMessage = catchAsync(async (req, res, next) => {
   await Message.create({
@@ -12,5 +23,12 @@ exports.postMessage = catchAsync(async (req, res, next) => {
     message: req.body.message
   });
 
+  res.redirect('/');
+});
+
+exports.deleteMessage = catchAsync(async (req, res, next) => {
+  await Message.findByIdAndDelete(req.params.messageId);
+
+  req.flash('info', 'Successfully deleted the message');
   res.redirect('/');
 });
