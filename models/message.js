607b3f8d83337d6d4e8475b1ba7c48c150eed73b
nevz <projectnevz@gmail.com>
2022-07-26 21:33:16 +0800

    Update message.js
    - Added a createdAt field
    
    - Added virtual for formatting createdAt to human readable date
    
    - Added virtual for getting the relative time of createdAt which is also human readable

--- models/message.js
+++ models/message.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const MessageSchema = new mongoose.Schema({
   user: {
@@ -17,7 +18,21 @@ const MessageSchema = new mongoose.Schema({
     required: true,
     minLength: 1,
     maxLength: 1000
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
+});
+
+MessageSchema.virtual('formattedDate').get(function () {
+  return DateTime.fromJSDate(this.createdAt).toLocaleString(
+    DateTime.DATETIME_MED
+  );
+});
+
+MessageSchema.virtual('relativeTime').get(function () {
+  return DateTime.fromJSDate(this.createdAt).toRelativeCalendar();
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
