1ec5a5258f76319a311e48cdb82510f49d8fd7e2
nevz <projectnevz@gmail.com>
2022-07-27 15:16:07 +0800

    Update messageController.js
    - Change the order of populate to be the last
    
    - Added page property in locals
    
    - Added a flash in getMessageForm
    -

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,15 +3,19 @@ const { catchAsync } = require('../utils/catchAsync');
 const { paginate } = require('../utils/paginate');
 
 exports.getAllMessages = catchAsync(async (req, res, next) => {
-  const skip = paginate(req.query.page || 1);
+  const skip = paginate(req.query.page ?? 1);
   const messages = await Message.find()
-    .populate({ path: 'user', select: '-password -email' })
     .skip(skip)
     .limit(10)
     .sort('-createdAt')
+    .populate({ path: 'user', select: '-password -email' })
     .exec();
 
-  res.render('index', { messages, info: req.flash('info')[0] });
+  res.render('index', {
+    messages,
+    info: req.flash('info')[0],
+    page: Number(req.query.page ?? 1)
+  });
 });
 
 exports.getMessageForm = (req, res) => res.render('new-message');
@@ -23,6 +27,7 @@ exports.postMessageForm = catchAsync(async (req, res, next) => {
     message: req.body.message
   });
 
+  req.flash('info', `Successfully posted ${req.body.title}`);
   res.redirect('/');
 });
 
